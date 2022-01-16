8146785b2c8c1b912dc5e8a8d5d358c1e18efa87
Aabhas Gupta <aabhasgupta97@gmail.com>
2022-01-16 19:21:44 +0530

    Code refactor for tries

--- strings/trie.cpp
+++ strings/trie.cpp
@@ -1,12 +1,13 @@
 #include<bits/stdc++.h>
-#include<unordered_map>
 using namespace std;
 const int MAX = 26;
+
 struct TrieNode{
     TrieNode* children[MAX];
     bool isEnd;
     int weight;
 };
+
 TrieNode* newnode(){
     TrieNode* root = new TrieNode();
     for(int i=0;i<MAX;i++){
@@ -16,28 +17,7 @@ TrieNode* newnode(){
     root->weight = 0;
     return root;
 }
-int findMax(TrieNode* node, int &maxval){
-    TrieNode* root = node;
-    if(root->isEnd)
-        maxval = max(maxval, root->weight);
-    int flag = 0;
-    for(int i=0;i<MAX;i++){
-        if(root->children[i]){
-            flag = 1;
-        }
-    }
-    if(!flag){
-        return max(root->weight, maxval);
-    }
-    else{
-        for(int i=0;i<MAX;i++){
-            if(root->children[i]){
-                maxval = max(maxval, findMax(root->children[i], maxval));
-            }
-        }
-    }
-    return maxval;
-}
+
 void insert(string s, int w, TrieNode* node){
     TrieNode* root = node;
     for(unsigned int i=0;i<s.length();i++){
@@ -50,40 +30,27 @@ void insert(string s, int w, TrieNode* node){
     root->isEnd = true;
     root->weight = w;
 }
-int search(string s, TrieNode* node){
+
+bool search(string s, TrieNode* node){
     TrieNode* root = node;
     for(unsigned int i=0;i<s.length();i++){
         int idx = s[i] - 'a';
         if(!root->children[idx]){
-            return -1;
+            return false;
         }
         root = root->children[idx];
     }
-    int ans = -1;
-    ans = findMax(root, ans);
-    return ans;
+    return true;
 }
+
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n,q,w;
-    unordered_map<string, int> mp;
-    cin>>n>>q;
+    int n,w;
     string s;
+    cin>>n;
     TrieNode* root = newnode();
     while(n--){
         cin>>s>>w;
         insert(s, w, root);
     }
-    while(q--){
-        cin>>s;
-        if(mp.find(s)!=mp.end()){
-            cout<<mp[s]<<endl;
-        }
-        else{
-            int ans = search(s, root);
-            mp[s] = ans;
-            cout<<ans<<endl;
-        }
-    }
+    return 0;
 }
